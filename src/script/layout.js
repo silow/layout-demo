d910217a7a36b3d053f2b17faf1d6f262deb5b5d
silow <silow@126.com>
2018-03-22 17:00:40 +0800

    20180322

--- src/script/layout.js
+++ src/script/layout.js
@@ -12,7 +12,7 @@
                                 ButtonHtml('trash','trash') +
                                 ButtonHtml('clipboard') +
                                 ButtonHtml('plus','addColumn') +
-                                '<h4>行</h4>' +
+                                '<h4>Row</h4>' +
                             '</div>' +
                             '<div class="row-body">'+
                             '</div></div>';
@@ -25,19 +25,27 @@
                                 ButtonHtml('trash','trash') +
                                 ButtonHtml('clipboard') +
                                 ButtonHtml('puzzle-piece','addComponent') +
-                                '<h5>列 1</h5>' +
+                                '<h5>Column</h5>' +
                             '</div>' +
                             '<div class="col-body">'+
                             '</div></div>';
             t.append(tpl);
         },
         addComponent:function(e){
+            initModal();
+            var body=$(this).closest('[data-type]').find('.col-body')[0];
             $('[data-type="modal"]').one('addComponent',$.proxy(function(e){
-                console.log(this);
-            },this)).show();
-            },
-        addSubRow:function(e){
-
+                var comp =Array.prototype.slice.call(arguments,1),
+                    fragment = document.createDocumentFragment();
+                if($('#subrow').prop('checked')&&comp.length > 1){
+                    fragment = $('<div class="col-subrow"><div class="card-title">SubRow</div></div>');
+                }
+                for(var i = 0; i<comp.length ; ++i){
+                    buildComponent(comp[i],fragment);
+                }
+                $(this).append(fragment);
+                $('[data-type="modal"]').hide();
+            },body)).show();
         },
         trash:function(e){
             $(this).closest('[data-type]').remove();
@@ -47,14 +55,25 @@
         },
         selectComponent:function(e){
             var subrow = $('#subrow').prop('checked');
-            if(subrow){
-                $('<span>',{text:$(this).text()}).appendTo('.subrow-com-list');
-            }else{
-                $(this).addClass('active').siblings().removeClass('active');
-            }
+            buildSubRowItem(this,'.subrow-com-list')
         },
         createSubRow:function(){
-            $(this).closest('fieldset').toggleClass('createRow').find('.subrow-com-list').toggle();
+            
+        },
+        subrowComponent:function(){
+            $(this).remove();
+        },
+        addCompToPage:function(){
+            var modal = $('[data-type="modal"]'),
+                comps = [];
+
+            modal.find('.subrow-com-list>span').each(function(){
+                comps.push($(this).text());
+            })
+            if(0 == comps.length){
+                return alert("请选择组件进行添加");
+            }
+            modal.triggerHandler('addComponent',comps);
         }
     }
     function ButtonHtml(icon,action){
@@ -62,5 +81,27 @@
         return  '<button class="prop-list-btn" data-action="'+action+'">' +
                     '<span class="oi" data-glyph="'+icon+'"></span>' +
                 '</button>';
+    }
+    function buildSubRowItem(el,target){
+        $('<span>',{
+            text:$(el).text(),
+            class:'subrow-com',
+            "data-component":$(el).text(),
+            "data-action":'subrowComponent'
+        }).appendTo(target);
+    }
+    function buildComponent(text,target){
+        var tpl = '<div class="component-com">' +
+                        ButtonHtml('trash','trash') +
+                        ButtonHtml('clipboard') +
+                        '<h6>'+text+'</h6>' +
+                    '</div>';
+        $(target).append(tpl);
+    }
+    function initModal(){
+        var modal = $('[data-type="modal"]');
+        modal.find('.active').removeClass('active');
+        modal.find(':checkbox').prop('checked',false);
+        modal.find('.subrow-com-list').empty();
     }
 }(jQuery)
